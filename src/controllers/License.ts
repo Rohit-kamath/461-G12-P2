7e496f3e343f5467149395e39a0889100ce6c878
Rohit Sreenivas Kamath <kamath7@eceprog2.ecn.purdue.edu>
2023-10-15 02:04:05 -0400

    updated license and BusFactor to use api.utils.ts

--- src/controllers/License.ts
+++ src/controllers/License.ts
@@ -1,34 +1,14 @@
-import { MetricParent } from '../helpers/MetricParent';
-import axios from 'axios';
-import dotenv from 'dotenv';
-dotenv.config();
+import { getRequest } from '../utils/api.utils';
 
-export class License extends MetricParent {
-  private repoOwner: string;
-  private repoName: string;
-
-  constructor(someSharedProperty: string, repoOwner: string, repoName: string) {
-    super(someSharedProperty, 'License Scorer', 'kim3574');
-    this.repoOwner = repoOwner;
-    this.repoName = repoName;
-  }
-  
-  async fetchLicenseScore(): Promise<number> {
-    const url = `https://api.github.com/repos/${this.repoOwner}/${this.repoName}/readme`;
-    try{
-      const response = await axios.get(url, {
-        headers: {
-          Authorization: `token ${process.env.GITHUB_TOKEN}`,
-        },
-      });
-      const readmeContent = response.data;
-      const readMe = Buffer.from(readmeContent.content, 'base64').toString('utf-8');
-      const licenseRegex = /licen[sc]e/gi;
-      const match = licenseRegex.test(readMe)
-      return match ? 1 : 0;
-    } catch (error) {
-      console.log(error)
-      return 0;
-    }
+export async function getLicenseScore(owner : string, repo : string): Promise<number> {
+  try{
+    const response = await getRequest(`/repos/${owner}/${repo}/readme`);
+    const readMe = Buffer.from(response.content, 'base64').toString();
+    const licenseRegex = /licen[sc]e/gi;
+    const match = licenseRegex.test(readMe)
+    return match ? 1 : 0;
+  } catch (error) {
+    console.log(error)
+    return 0;
   }
 }
