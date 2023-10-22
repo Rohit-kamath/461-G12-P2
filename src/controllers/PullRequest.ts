965d3b07b7c80bc11fbbb3d3519be7607976d375
Rohit Sreenivas Kamath <kamath7@eceprog2.ecn.purdue.edu>
2023-10-22 14:41:20 -0400

    tried implementing PullRequest metric

---
+++ src/controllers/PullRequest.ts
@@ -0,0 +1,25 @@
+import { getRequest } from "../utils/api.utils";
+
+//find the total number of commits from a repo
+export async function numberOfCommits(owner : string, repo : string): Promise<number> {
+    const response = await getRequest(`/repos/${owner}/${repo}/commits`);
+    return response.length;
+}
+
+//find the number of approved pull requests from a repo
+export async function numberOfApprovedPRs(owner: string, repo : string): Promise<number>{
+    const response = await getRequest(`/repos/${owner}/${repo}/pulls?state=all`);
+    let approvedPRs = 0;
+    response.forEach((pr: any) => {
+        if(pr.merged_at){
+            approvedPRs++;
+        }
+    });
+    return approvedPRs;
+}
+
+export async function getPullRequest(owner: string, repo : string): Promise<number> {
+    const approvedPRs = await numberOfApprovedPRs(owner, repo);
+    const commits = await numberOfCommits(owner, repo);
+    return (approvedPRs)/commits;
+}
