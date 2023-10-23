bc2072d5b5f03bba52ad998fa240e9a20552c7c7
rvchit <rachitb10@gmail.com>
2023-10-23 00:22:11 -0400

    Rampup w/ tests fixed

--- __test__/RampUp_Integration.test.ts
+++ __test__/RampUp_Integration.test.ts
@@ -1,17 +1,15 @@
-import { calculateRampUp } from '../src/controllers/RampUp'; 
+import { calculateRampUp } from '../src/controllers/RampUp';
 
 describe('RampUp Calculations Integration Tests', () => {
+	const owner = 'facebook';
+	const repo = 'react';
 
-    const owner = 'facebook';
-    const repo = 'react';
+	test('Fetching and computing ramp-up score for a known repository', async () => {
+		const rampUpScore = await calculateRampUp(owner, repo);
 
-    test('Fetching and computing ramp-up score for a known repository', async () => {
-        const rampUpScore = await calculateRampUp(owner, repo);
-        
-        expect(rampUpScore).toBeGreaterThanOrEqual(0);
-        expect(rampUpScore).toBeLessThanOrEqual(1);
-    });
+		expect(rampUpScore).toBeGreaterThanOrEqual(0);
+		expect(rampUpScore).toBeLessThanOrEqual(1);
+	});
 
-    // will add more integration tests
-
+	// will add more integration tests
 });
--- __test__/RampUp_Unit.test.ts
+++ __test__/RampUp_Unit.test.ts
@@ -1,31 +1,47 @@
 import * as RampUpAPI from '../src/utils/RampUpAPI';
 import { calculateRampUp } from '../src/controllers/RampUp';
 
-jest.mock('../src/utils/RampUpAPI');  // Mock the entire RampUpAPI module
+jest.mock('../src/utils/RampUpAPI'); // Mock the entire RampUpAPI module
 
 describe('RampUp Unit Tests', () => {
-    const owner = 'facebook';
-    const repo = 'react';
+	const owner = 'facebook';
+	const repo = 'react';
 
-    beforeEach(() => {
-        // Reset all mocks before each test
-        jest.clearAllMocks();
-    });
+	beforeEach(() => {
+		// Reset all mocks before each test
+		jest.clearAllMocks();
+	});
 
-    it('should correctly compute a ramp-up score based on mock data', async () => {
-        // Mocking the data fetched from GitHub API
-        (RampUpAPI.fetchRepositoryContributors as jest.Mock).mockImplementation(() => Promise.resolve([{ /*...mock contributor data...*/ }]));
-        (RampUpAPI.fetchRepositoryStars as jest.Mock).mockImplementation(() => Promise.resolve([{ /*...mock star data...*/ }]));
-        (RampUpAPI.fetchRepositoryForks as jest.Mock).mockImplementation(() => Promise.resolve([{ /*...mock fork data...*/ }]));
-        (RampUpAPI.fetchFirstCommitTime as jest.Mock).mockImplementation(() => Promise.resolve('2022-01-01T10:00:00Z'));
+	it('should correctly compute a ramp-up score based on mock data', async () => {
+		// Mocking the data fetched from GitHub API
+		(RampUpAPI.fetchRepositoryContributors as jest.Mock).mockImplementation(() =>
+			Promise.resolve([
+				{
+					/*...mock contributor data...*/
+				},
+			]),
+		);
+		(RampUpAPI.fetchRepositoryStars as jest.Mock).mockImplementation(() =>
+			Promise.resolve([
+				{
+					/*...mock star data...*/
+				},
+			]),
+		);
+		(RampUpAPI.fetchRepositoryForks as jest.Mock).mockImplementation(() =>
+			Promise.resolve([
+				{
+					/*...mock fork data...*/
+				},
+			]),
+		);
+		(RampUpAPI.fetchFirstCommitTime as jest.Mock).mockImplementation(() => Promise.resolve('2022-01-01T10:00:00Z'));
 
-        const rampUpScore = await calculateRampUp(owner, repo);
+		const rampUpScore = await calculateRampUp(owner, repo);
 
-        expect(rampUpScore).toBeGreaterThanOrEqual(0);
-        expect(rampUpScore).toBeLessThanOrEqual(1);
-    });
+		expect(rampUpScore).toBeGreaterThanOrEqual(0);
+		expect(rampUpScore).toBeLessThanOrEqual(1);
+	});
 
-    // will add more unit tests here
-
+	// will add more unit tests here
 });
-
