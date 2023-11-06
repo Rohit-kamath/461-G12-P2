a9ef63edf26db35cf03b8b22ed79699beb8ed2e0
Rohit Sreenivas Kamath <kamath7@eceprog3.ecn.purdue.edu>
2023-11-06 01:04:56 -0500

    made popularity metrics and made a union type for regEx search. Gonna wait on doing it for the package directory thing for now

---
+++ __backend_test__/getPackagePopularity.test.ts
@@ -0,0 +1,19 @@
+import { getPackagePopularity } from '../backend/apiPackage';
+
+describe('getPackagePopularity', () => {
+    it('should return package popularity when valid data is provided', async () => {
+        const url = "https://github.com/cloudinary/cloudinary_npm";
+        const popularity = await getPackagePopularity(url);
+        console.log(popularity);
+        expect(popularity.stars).toBeGreaterThan(500);
+        expect(popularity.forks).toBeGreaterThan(300);
+    });
+    it('should return package popularity even for a npm url', async () => {
+        const url = "https://www.npmjs.com/package/express";
+        const popularity = await getPackagePopularity(url);
+        console.log(popularity);
+        expect(popularity.stars).toBeGreaterThan(500);
+        expect(popularity.forks).toBeGreaterThan(300);
+    });
+}
+);
