1957959be7a9b44676bb6b27add9e555c4de0611
Rohit Sreenivas Kamath <kamath7@eceprog2.ecn.purdue.edu>
2023-12-02 17:56:40 -0500

    updated packageUpdate test and it works

--- __API_test__/putPackageByID.test.ts
+++ __API_test__/putPackageByID.test.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-// import * as apiSchema from "../backend/apiSchema";
+import * as apiSchema from "../backend/apiSchema";
 
 const APIURL = 'http://ece461-packageregistry-depenv.eba-bphpcw3d.us-east-2.elasticbeanstalk.com';
 describe('reset', () => {
@@ -32,39 +32,39 @@ describe('putPackageByID', () => {
             expect(error.response.status).toBe(404);
         }
     });
-    // let metadata : apiSchema.PackageMetadata;
-    // it('POST /package endpoint to put something in registry. should return 200 status code and something for a valid github repo link', async () => {
-    //     try {
-    //         const response = await axios.post(`${APIURL}/package`, {
-    //             "URL": "https://www.npmjs.com/package/express",
-    //             "JSProgram": "if (process.argv.length === 7) {\nconsole.log('Success')\nprocess.exit(0)\n} else {\nconsole.log('Failed')\nprocess.exit(1)\n}\n"
-    //         });
-    //         expect(response.status).toBe(200);
-    //         metadata = response.data.metadata;
-    //     } catch (error: any) {
-    //         console.log(error.response.status);
-    //         throw error;
-    //     }
-    // });
+    let metadata : apiSchema.PackageMetadata;
+    it('POST /package endpoint to put something in registry. should return 200 status code and something for a valid github repo link', async () => {
+        try {
+            const response = await axios.post(`${APIURL}/package`, {
+                "URL": "https://www.npmjs.com/package/express",
+                "JSProgram": "if (process.argv.length === 7) {\nconsole.log('Success')\nprocess.exit(0)\n} else {\nconsole.log('Failed')\nprocess.exit(1)\n}\n"
+            });
+            expect(response.status).toBe(200);
+            metadata = response.data.metadata;
+        } catch (error: any) {
+            console.log(error.response.status);
+            throw error;
+        }
+    });
 
-    // it('should return 200 status code to signifiy successful put', async () => {
-    //     try {
-    //         const response = await axios.put(`${APIURL}/package/${metadata.ID}`, {
-    //             "metadata": {
-    //                 "Name": metadata.Name,
-    //                 "Version": metadata.Version,
-    //                 "ID": metadata.ID,
-    //             },
-    //             "data": {
-    //                 "Content": 'test',
-    //             }
-    //         }); 
-    //         expect(response.status).toBe(200);
-    //     } catch (error: any) {
-    //         console.log(error.response.status);
-    //         throw error;
-    //     }
-    // });
+    it('should return 200 status code to signifiy successful put', async () => {
+        try {
+            const response = await axios.put(`${APIURL}/package/${metadata.ID}`, {
+                "metadata": {
+                    "Name": metadata.Name,
+                    "Version": metadata.Version,
+                    "ID": metadata.ID,
+                },
+                "data": {
+                    "Content": 'test',
+                }
+            }); 
+            expect(response.status).toBe(200);
+        } catch (error: any) {
+            console.log(error.response.status);
+            throw error;
+        }
+    });
 });
 
 describe('always pass', () => {
